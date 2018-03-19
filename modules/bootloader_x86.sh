f78b641286711fe42a0fe88ed9c680671a77fa88
Forrest Feng <changzheng.feng@carestream.com>
2018-03-19 18:23:38 +0800

    Fix bug (1) - Tried 20180315 livecd, grub:2 uses the same command as grub version one: grub_install not grub2_install, grub-mkconfig not grub2-mkconfig

--- modules/bootloader_x86.sh
+++ modules/bootloader_x86.sh
@@ -55,7 +55,7 @@ configure_bootloader_grub2() {
     debug configure_bootloader_grub2 "configuring and deploying grub2"
 
     check_chroot_fstab /boot && spawn_chroot "[ -z \"\$(mount | grep /boot)\" ] && mount /boot"            
-
+    # 20180315 grub:2 use the same command as grub version one: grub_install not grub2_install, grub-mkconfig not grub2-mkconfig
     [ -z "${!grub2_install[@]}" ] && warn "looks like it's pulling grub:2 but 'grub2_install' is not set... is it intended?"
     for device in "${!grub2_install[@]}"; do
         # FIXME only accepts a single option currently (--modules=)        
@@ -64,10 +64,10 @@ configure_bootloader_grub2() {
     
         if [ -n "${key}" ] && [ -n "${value}" ]; then 
             debug configure_bootloader_grub2 "deploying grub2-install $key=$value /dev/${device}"
-            spawn_chroot "grub2-install ${key}=${value} /dev/${device}" || die "Could not deploy grub2-install $key=$value /dev/${device}"
+            spawn_chroot "grub-install ${key}=${value} /dev/${device}" || die "Could not deploy grub2-install $key=$value /dev/${device}"
         else
             debug configure_bootloader_grub2 "deploying grub2-install /dev/${device}"
-            spawn_chroot "grub2-install /dev/${device}" || die "Could not deploy grub2-install /dev/${device}"
+            spawn_chroot "grub-install /dev/${device}" || die "Could not deploy grub2-install /dev/${device}"
         fi
         #spawn_chroot "grub2-install --modules=\"part_gpt mdraid1x lvm xfs\" /dev/sda" || die "Could not deploy with grub2-install on /dev/sda"
         #spawn_chroot "grub2-install --modules=\"part_gpt mdraid1x lvm xfs\" /dev/sdb" || die "Could not deploy with grub2-install on /dev/sdb"
@@ -83,7 +83,7 @@ configure_bootloader_grub2() {
 	spawn "echo -e '\n\nGRUB_CMDLINE_LINUX=\"\$GRUB_CMDLINE_LINUX ${args}\"' >> ${chroot_dir}/etc/default/grub" || die "Could not add dolvm option to ${chroot_dir}/etc/default/grub"
     fi
     debug configure_grub2 "generating /boot/grub/grub.cfg"
-    spawn_chroot "grub2-mkconfig -o /boot/grub/grub.cfg" || die "Could not generate /boot/grub2/grub.cfg"
+    spawn_chroot "grub-mkconfig -o /boot/grub/grub.cfg" || die "Could not generate /boot/grub2/grub.cfg"
 }
 
 # made for funtoo, broken for gentoo
